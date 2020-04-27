539d282183e60c108a6a27654d469cdbba53209c
Joseph Flores <yosept.flores@gmail.com>
2020-04-27 15:11:01 -0500

    The camera follows the rocket correctly, and the ground has a tile animation

--- src/index.js
+++ src/index.js
@@ -1,68 +1,67 @@
 import Phaser from 'phaser';
 import './sass/style.scss';
 import ball from './images/ball.png';
-import sky from './images/sky1.png';
+import sky from './images/sky.png';
+import ground from './images/grounds.png';
+import transparent from './images/transparent.png';
 
 let ballA;
-let ballB;
+let grounded;
+let groundedTexture;
 let background;
-const iter = 0;
 
 function preload() {
   this.load.image('ball', ball);
   this.load.image('sky', sky);
+  this.load.image('ground', ground);
+  this.load.image('transparent', transparent);
 }
 
 function create() {
-  this.matter.world.setBounds(0, 0, 800, 600, 64, false, false, false, true);
   background = this.add.tileSprite(400, 300, 1920, 1080, 'sky');
-
   background.setScrollFactor(0, 0);
+  groundedTexture = this.add.tileSprite(400, 2015, 500, 200, 'ground');
+
+  console.log(groundedTexture.x);
+
+  grounded = this.matter.add.image(100, 1980, 'transparent', null, {
+    shape: 'rectangle', friction: 0.005, restitution: 0.6, isStatic: true,
+  });
+
+  ballA = this.matter.add.image(420, 100, 'ball', null, { shape: 'circle', friction: 0.005, restitution: 0.6 });
+
 
   //  Our two bodies which will be connected by a constraint (aka a Joint or a Spring)
 
-  ballA = this.matter.add.image(420, 100, 'ball', null, { shape: 'circle', friction: 0.005, restitution: 0.6 });
-  ballB = this.matter.add.image(400, 200, 'ball', null, { shape: 'circle', friction: 0.005, restitution: 0.6 });
 
-  console.log(Phaser.Physics.Matter.Matter.Render);
-
-  //  You can create a constraint between the two bodies using a Factory function.
-  //  The value 100 is the resting length and 0.2 is the stiffness of the constraint.
-
-  //   this.matter.add.constraint(ballA, ballB, 100, 0.2);
-
-  //  To help those of you more used to the Box2D syntax you can use
-  //  add.joint or add.spring instead (with the exact same parameters)
-
-  // this.matter.add.spring(ballA, ballB, 100, 0.2);
-  // this.matter.add.joint(ballA, ballB, 100, 0.2);
-
-  //  Or you can create a native Matter constraint:
-
-  const constraint = Phaser.Physics.Matter.Matter.Constraint.create({
-    bodyA: ballA.body,
-    bodyB: ballB.body,
-    length: 100,
-    stiffness: 0.2,
-  });
-
-  //  Which you then have to add to the world yourself:
-
-  this.matter.world.add(constraint);
+  // const constraint = Phaser.Physics.Matter.Matter.Constraint.create({
+  //   bodyA: ballA.body,
+  //   bodyB: ballB.body,
+  //   length: 100,
+  //   stiffness: 0.2,
+  // });
+  //
+  // this.matter.world.add(constraint);
 
   this.matter.add.mouseSpring();
 }
 
+const times = 0;
+
 function update() {
-  this.cameras.main.setBounds(ballA.x - 400, ballA.y - 400, 800, 600, true);
+  console.log(this.cameras.main.x);
+  this.cameras.main.setBounds(ballA.x - 425, ballA.y - 700, 840, 450, true);
   background.tilePositionX = ballA.x;
   background.tilePositionY = ballA.y;
+  groundedTexture.tilePositionX = ballA.x;
+  groundedTexture.x = ballA.x;
+  grounded.x = ballA.x - 200;
 }
 
 const config = {
   type: Phaser.AUTO,
-  width: 800,
-  height: 600,
+  width: 450,
+  height: 840,
   backgroundColor: '#1b1464',
   parent: 'phaser-example',
   physics: {
